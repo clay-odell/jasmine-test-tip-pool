9a1530bb8fec8b2c77767eb53b68f3270ccb4c53
Clay O'Dell <clay.odell.music@gmail.com>
2024-01-29 00:17:04 -0600

    first two function tests corrected

--- payments.test.js
+++ payments.test.js
@@ -13,29 +13,22 @@ describe("Payments test (with setup and tear-down)", function() {
         expect(allPayments['payment1'].tipAmt).toEqual('20');
         expect(allPayments['payment1'].tipPercent).toEqual(20);
       });
-    it('should return undefined with negative or empty inputs and tip can be zero, but positive bill amount is required for createCurPayment()', () => {
+    
+    it('should not add a new payment on submitPaymentInfo() with empty input', function () {
+            billAmtInput.value = '';
+            submitPaymentInfo();
+        
+            expect(Object.keys(allPayments).length).toEqual(0);
+          });
+    afterEach( () => {
         billAmtInput.value = '';
         tipAmtInput.value = '';
-        let payment = createCurPayment();
-        expect(payment).toBeUndefined();
-        billAmtInput.value = -100;
-        tipAmtInput.value = -20
-        payment = createCurPayment();
-        expect(payment).toBeUndefined();
-    });
-    it('should create table row element and pass to appendTd with input value', () => {
+        paymentTbody.innerHTML = '';
+        summaryTds[0].innerHTML = '';
+        summaryTds[1].innerHTML = '';
+        summaryTds[2].innerHTML = '';
+        allPayments = {};
+        paymentId = 0;
+    })
     
-       })
-
-        afterEach(function() {
-            billAmtInput.value = '';
-            tipAmtInput.value = '';
-            paymentTbody.innerHTML = '';
-            summaryTds[0].innerHTML = '';
-            summaryTds[1].innerHTML = '';
-            summaryTds[2].innerHTML = '';
-            serverTbody.innerHTML = '';
-            paymentId = 0;
-            allPayments = {};
-          });
 });
