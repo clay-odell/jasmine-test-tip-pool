68b7d10bc1e7cf847a9fe08a0b6d4b6fc3516816
Clay O'Dell <clay.odell.music@gmail.com>
2024-01-29 10:49:17 -0600

    test function for submitPaymentInfo added, along with afterEach to clear the DOM

--- payments.test.js
+++ payments.test.js
@@ -1,6 +1,25 @@
 describe("Payments test (with setup and tear-down)", function() {
-    beforeEach(function () {
+    beforeEach(() => {
       billAmtInput.value = 100;
       tipAmtInput.value = 20;
+    });
+
+    it('should add a curPayment object to allPayments, update html and reset input values', () => {
+       submitPaymentInfo();
+       expect(Object.keys(allPayments).length).toEqual(1);
+       expect(allPayments['payment1'].billAmt).toEqual('100');
+       expect(allPayments['payment1'].tipAmt).toEqual('20');
+       expect(allPayments['payment1'].tipPercent).toEqual(20);
+    });
+    afterEach(() => {
+        billAmtInput.value = '';
+        tipAmtInput.value = '';
+        paymentForm.reset();
+        paymentTbody.innerHTML = '';
+        summaryTds[0].innerHTML = '';
+        summaryTds[1].innerHTML = '';
+        summaryTds[2].innerHTML = '';
+        allPayments = {};
+        paymentId = 0;
     });
 });
