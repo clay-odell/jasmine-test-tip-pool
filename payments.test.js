63311f05cfa870d79b3388929cb1b02829f9d7f4
Clay O'Dell <clay.odell.music@gmail.com>
2024-01-29 11:04:52 -0600

    added test for createCurPayment () function

--- payments.test.js
+++ payments.test.js
@@ -11,6 +11,13 @@ describe("Payments test (with setup and tear-down)", function() {
        expect(allPayments['payment1'].tipAmt).toEqual('20');
        expect(allPayments['payment1'].tipPercent).toEqual(20);
     });
+
+    it('should not add a new payment on submitPaymentInfo() with empty input', () => {
+        billAmtInput.value = '';
+        submitPaymentInfo();
+        expect(Object.keys(allPayments).length).toEqual(0);
+    });
+
     afterEach(() => {
         billAmtInput.value = '';
         tipAmtInput.value = '';
